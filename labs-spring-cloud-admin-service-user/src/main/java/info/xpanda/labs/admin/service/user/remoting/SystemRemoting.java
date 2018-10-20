3dbc6468f9a54b830ec26d92e940b22ea1e753ae
pauljoo <huiyaojiang@gmail.com>
2018-10-20 18:14:57 +0800

    更新system微服务

--- labs-spring-cloud-admin-service-user/src/main/java/info/xpanda/labs/admin/service/user/remoting/SystemRemoting.java
+++ labs-spring-cloud-admin-service-user/src/main/java/info/xpanda/labs/admin/service/user/remoting/SystemRemoting.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "labs-spring-cloud-service-system", fallback = SystemRemotingHystrix.class)
+@FeignClient(name = "labs-spring-cloud-admin-service-system", fallback = SystemRemotingHystrix.class)
 @RequestMapping("/system")
 public interface SystemRemoting {
     @GetMapping("/info")
