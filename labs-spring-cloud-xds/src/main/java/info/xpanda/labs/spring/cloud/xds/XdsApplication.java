85716bf2f50ff518834ee3049bb3e015637af17a
pauljoo <huiyaojiang@gmail.com>
2020-02-11 20:45:56 +0800

    11111

--- labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/XdsApplication.java
+++ labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/XdsApplication.java
@@ -1,13 +1,13 @@
 package info.xpanda.labs.spring.cloud.xds;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @author Paul Jiang
  * @since 20200211
  */
-@SpringBootApplication
+@SpringCloudApplication
 public class XdsApplication {
     public static void main(String[] args) {
         SpringApplication.run(XdsApplication.class, args);
--- labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/controller/XdsController.java
+++ labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/controller/XdsController.java
@@ -1,9 +1,14 @@
 package info.xpanda.labs.spring.cloud.xds.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 /**
  * @author Paul Jiang
@@ -12,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class XdsController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/v2/discovery:clusters")
-    public String cluster(@RequestBody(required = false) String body){
+    public String cluster(HttpServletRequest request, @RequestBody(required = false) String body){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            log.info("header:" + headerName + "=" + request.getHeader(headerName));
+        }
         log.info(body);
         return "";
     }
