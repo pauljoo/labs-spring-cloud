42c818bbf6327dd37c7cc84561c5928b6bd1ecb3
pauljoo <huiyaojiang@gmail.com>
2020-02-11 18:57:47 +0800

    11111

---
+++ labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/XdsApplication.java
@@ -0,0 +1,11 @@
+package info.xpanda.labs.spring.cloud.xds;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Paul Jiang
+ * @since 20200211
+ */
+@SpringBootApplication
+public class XdsApplication {
+}
