590456000f29642dc75a8b91ad8e19a2f96b138f
pauljoo <huiyaojiang@gmail.com>
2020-02-11 16:34:51 +0800

    11111

--- labs-spring-cloud-service-envoy/src/main/java/info/xpanda/labs/spring/cloud/service/envoy/controller/EnvoyController.java
+++ labs-spring-cloud-service-envoy/src/main/java/info/xpanda/labs/spring/cloud/service/envoy/controller/EnvoyController.java
@@ -3,7 +3,6 @@ package info.xpanda.labs.spring.cloud.service.envoy.controller;
 import info.xpanda.labs.spring.cloud.service.envoy.service.EnvoyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/envoy")
 public class EnvoyController {
-    @Value("${spring.cloud.client.ipAddress}")
-    private String ip;
-
     @Autowired
     private EnvoyService envoyService;
 
@@ -22,13 +18,13 @@ public class EnvoyController {
     @ResponseBody
     public String user(){
         String info = envoyService.user();
-        return "I'm Envoy Service!" + ip + info;
+        return "I'm Envoy Service!" + info;
     }
 
     @RequestMapping("/system")
     @ResponseBody
     public String system(){
         String info = envoyService.system();
-        return "I'm Envoy Service!" + ip + info;
+        return "I'm Envoy Service!"  + info;
     }
 }
