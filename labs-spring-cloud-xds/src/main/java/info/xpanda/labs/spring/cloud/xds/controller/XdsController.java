2fec115e8f3670026784e1e3af2259c5fb7e8703
pauljoo <huiyaojiang@gmail.com>
2020-02-11 20:16:50 +0800

    11111

--- labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/controller/XdsController.java
+++ labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/controller/XdsController.java
@@ -1,5 +1,6 @@
 package info.xpanda.labs.spring.cloud.xds.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 20200211
  */
 @RestController
+@Slf4j
 public class XdsController {
     @RequestMapping("/v2/discovery:clusters")
     public String cluster(@RequestBody(required = false) String body){
-        System.out.println(body);
+        log.info(body);
         return "";
     }
 }
