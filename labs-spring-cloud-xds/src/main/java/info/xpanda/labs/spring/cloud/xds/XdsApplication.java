fe4a4fb7d1fd869b986e4f2a537dc55cc321452b
pauljoo <huiyaojiang@gmail.com>
2020-02-12 13:57:35 +0800

    11111

--- labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/XdsApplication.java
+++ labs-spring-cloud-xds/src/main/java/info/xpanda/labs/spring/cloud/xds/XdsApplication.java
@@ -1,65 +1,18 @@
 package info.xpanda.labs.spring.cloud.xds;
 
-import com.google.common.collect.ImmutableList;
-import com.google.protobuf.Duration;
-import io.envoyproxy.controlplane.cache.SimpleCache;
-import io.envoyproxy.controlplane.cache.Snapshot;
-import io.envoyproxy.controlplane.server.DiscoveryServer;
-import io.envoyproxy.envoy.api.v2.Cluster;
-import io.envoyproxy.envoy.api.v2.core.Address;
-import io.envoyproxy.envoy.api.v2.core.SocketAddress;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.netty.NettyServerBuilder;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Paul Jiang
  * @since 20200211
  */
-@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
 public class XdsApplication {
-    private static final String GROUP = "key";
-
 
     public static void main(String[] args) {
-//        SimpleCache<String> cache = new SimpleCache<>(node -> GROUP);
-//
-//        cache.setSnapshot(
-//                GROUP,
-//                Snapshot.create(
-//                        ImmutableList.of(
-//                                Cluster.newBuilder()
-//                                        .setName("cluster0")
-//                                        .setConnectTimeout(Duration.newBuilder().setSeconds(5))
-//                                        .setType(Cluster.DiscoveryType.STATIC)
-//                                        .addHosts(Address.newBuilder()
-//                                                .setSocketAddress(SocketAddress.newBuilder().setAddress("127.0.0.1").setPortValue(1234)))
-//                                        .build()),
-//                        ImmutableList.of(),
-//                        ImmutableList.of(),
-//                        ImmutableList.of(),
-//                        ImmutableList.of(),
-//                        "1"));
-//
-//        DiscoveryServer discoveryServer = new DiscoveryServer(cache);
-//
-//        ServerBuilder builder = NettyServerBuilder.forPort(12345)
-//                .addService(discoveryServer.getAggregatedDiscoveryServiceImpl())
-//                .addService(discoveryServer.getClusterDiscoveryServiceImpl())
-//                .addService(discoveryServer.getEndpointDiscoveryServiceImpl())
-//                .addService(discoveryServer.getListenerDiscoveryServiceImpl())
-//                .addService(discoveryServer.getRouteDiscoveryServiceImpl());
-//
-//        Server server = builder.build();
-//
-//        server.start();
-//
-//        System.out.println("Server has started on port " + server.getPort());
-//
-//        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
-
         SpringApplication.run(XdsApplication.class, args);
     }
 
